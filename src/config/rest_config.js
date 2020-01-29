7c7518cd1a704e1d6806af343ddeafae43634d97
Muhammad_Usman <arain3027@gmail.com>
2020-01-29 20:12:44 +0500

    country code changed

--- src/config/rest_config.js
+++ src/config/rest_config.js
@@ -1,4 +1,4 @@
 export const articles_url = 'https://newsapi.org/v2/top-headlines';
-export const country_code = 'in';
+export const country_code = 'us';
 export const category = 'general';
 export const _api_key = '9b6991f185a546069f281f623b9763e1';
